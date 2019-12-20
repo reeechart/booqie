4fcd314b54aecfa4e92f877ecd275e3b17205184
Ferdinandus Richard <reeechart@gmail.com>
2019-12-20 16:49:03 +0700

    add http request parser for graphql

--- book/http/request/parser.go
+++ book/http/request/parser.go
@@ -1,6 +1,8 @@
 package request
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strings"
@@ -14,17 +16,50 @@ const (
 func ParseGraphQLRequest(r *http.Request) (*GraphQLParameter, error) {
 	switch r.Method {
 	case http.MethodGet:
-		return &GraphQLParameter{}, nil
+		return parseGetRequest(r)
 	case http.MethodPost:
-		contentType := r.Header.Get("Content-Type")
-		if strings.HasPrefix(contentType, contentTypeJson) {
-			return &GraphQLParameter{}, nil
-		} else if strings.HasPrefix(contentType, contentTypeGraphQL) {
-			return &GraphQLParameter{}, nil
-		} else {
-			return nil, errors.New("Unsupported content type")
-		}
+		return parsePostRequest(r)
 	default:
 		return nil, errors.New("Unsupported HTTP Method")
 	}
+}
+
+func parseGetRequest(r *http.Request) (*GraphQLParameter, error) {
+	gqlParam := GraphQLParameter{}
+	params := r.URL.Query()
+	query, queryOk := params["query"]
+	if queryOk {
+		gqlParam.Query = query[0]
+	}
+
+	return &GraphQLParameter{
+		Query: query[0],
+	}, nil
+}
+
+func parsePostRequest(r *http.Request) (*GraphQLParameter, error) {
+	contentType := r.Header.Get("Content-Type")
+	if strings.HasPrefix(contentType, contentTypeJson) {
+		return parseQueryFromJSON(r)
+	} else if strings.HasPrefix(contentType, contentTypeGraphQL) {
+		return parseQueryFromGraphQL(r)
+	} else {
+		return nil, errors.New("Unsupported content type")
+	}
+}
+
+func parseQueryFromJSON(r *http.Request) (*GraphQLParameter, error) {
+	gqlParam := GraphQLParameter{}
+	err := json.NewDecoder(r.Body).Decode(&gqlParam)
+	if err != nil {
+		return nil, err
+	}
+	return &gqlParam, nil
+}
+
+func parseQueryFromGraphQL(r *http.Request) (*GraphQLParameter, error) {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	query := buf.String()
+	return &GraphQLParameter{Query: query}, nil
 }
