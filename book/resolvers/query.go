f85b9c55da01433fba055d4b762f5c52771be275
Ferdinandus Richard <reeechart@gmail.com>
2019-12-20 17:40:59 +0700

    move dummybooks creation

--- book/resolvers/query.go
+++ book/resolvers/query.go
@@ -6,6 +6,27 @@ import (
 	"github.com/reeechart/booql/book/models"
 )
 
+var dummyBooks = []models.Book{
+	models.Book{
+		Id:    1,
+		Title: "Book 1",
+		Author: models.Author{
+			Id:   1,
+			Name: "Author 1",
+		},
+		Year: 2000,
+	},
+	models.Book{
+		Id:    2,
+		Title: "Book 2",
+		Author: models.Author{
+			Id:   2,
+			Name: "Author 2",
+		},
+		Year: 2001,
+	},
+}
+
 type QueryResolver struct {
 }
 
@@ -14,33 +35,10 @@ func NewQueryResolver() *QueryResolver {
 }
 
 func (query *QueryResolver) GetBooks() *[]*BookResolver {
-	dummyBooks := []models.Book{
-		models.Book{
-			Id:    1,
-			Title: "Book 1",
-			Author: models.Author{
-				Id:   1,
-				Name: "Author 1",
-			},
-			Year: 2000,
-		},
-		models.Book{
-			Id:    2,
-			Title: "Book 2",
-			Author: models.Author{
-				Id:   2,
-				Name: "Author 2",
-			},
-			Year: 2001,
-		},
-	}
-
 	return &[]*BookResolver{
 		&BookResolver{&dummyBooks[0]},
 		&BookResolver{&dummyBooks[1]},
 	}
-	// parse query
-	// handler.schema.exec (processed by resolver)
 }
 
 func (query *QueryResolver) GetAuthors() *[]*AuthorResolver {
