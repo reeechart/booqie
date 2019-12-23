689000ee30eb213fd764ba3558a8f6722bc1b782
Ferdinandus Richard <reeechart@gmail.com>
2019-12-23 17:12:29 +0700

    update database config fetching

--- book/config/database_config.go
+++ book/config/database_config.go
@@ -14,13 +14,13 @@ type databaseConfig struct {
 	ssl    string
 }
 
-func GetConnectionString() (string, error) {
+func GetDatabaseConnectionString() string {
 	viper, err := getConfig()
 	if err != nil {
-		return "", err
+		panic(err)
 	}
 	dbConfig := getDatabaseConfig(viper)
-	return fmt.Sprintf("user=%s dbname=%s host=%s port=%s sslmode=%s", dbConfig.user, dbConfig.dbname, dbConfig.host, dbConfig.port, dbConfig.ssl), nil
+	return fmt.Sprintf("user=%s dbname=%s host=%s port=%s sslmode=%s", dbConfig.user, dbConfig.dbname, dbConfig.host, dbConfig.port, dbConfig.ssl)
 }
 
 func getDatabaseConfig(viper *viper.Viper) *databaseConfig {
