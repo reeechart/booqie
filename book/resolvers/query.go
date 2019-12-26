e18109be7e5983d822cb3b57e56613d8e8acf24b
Ferdinandus Richard <reeechart@gmail.com>
2019-12-26 14:41:53 +0700

    remove dummy books

--- book/resolvers/query.go
+++ book/resolvers/query.go
@@ -4,30 +4,8 @@ import (
 	"context"
 
 	"github.com/reeechart/booql/book/infra"
-	"github.com/reeechart/booql/book/models"
 	"github.com/reeechart/booql/book/repo"
 )
-
-var dummyBooks = []models.Book{
-	models.Book{
-		Id:    1,
-		Title: "Book 1",
-		Author: models.Author{
-			Id:   1,
-			Name: "Author 1",
-		},
-		Year: 2000,
-	},
-	models.Book{
-		Id:    2,
-		Title: "Book 2",
-		Author: models.Author{
-			Id:   2,
-			Name: "Author 2",
-		},
-		Year: 2001,
-	},
-}
 
 type QueryResolver struct {
 	authorRepo *repo.AuthorRepo
